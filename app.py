523058d9fbbd6b8daeb179cf6a63aafd00bc2bfa
brandonbeckmusic <brandonbeckmusic@gmail.com>
2024-12-10 15:19:58 -0500

    File additions for project.

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('vehicles_us.csv')
+
+st.header("Exploratory Data Analysis: Vehicle Data")
+
+st.write(df.head())
+
+fig = px.histogram(df, x='price', color='condition', nbins=50, title="Price Distribution by Condition")
+st.plotly_chart(fig)
+
+fig = px.histogram(df, x='odometer', color='is_4wd', nbins=50, title="Odometer Distribution by 4WD")
+st.plotly_chart(fig)
+
+fig = px.scatter(df, x='odometer', y='price', title='Price vs Odometer')
+st.plotly_chart(fig)
+
+fig = px.scatter(df, x='model_year', y='odometer', title="Odometer vs Model Year")
+st.plotly_chart(fig)
+
+is_4wd = st.checkbox("Show only 4WD vehicles")
+if is_4wd:
+    df = df[df['is_4wd'] == 1]
+st.write(df.head())
---
+++ sd-tools.py
@@ -0,0 +1,7 @@
+print("Hello, this is my project, and I'm gonna do my best.")
+import pandas as pd
+import streamlit as st
+import altair as alt
+import plotly_express as plty
+
+df = pd.read_csv('vehicles_us.csv')
